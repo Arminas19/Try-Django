017eaa9ab2e470ba0a4e0267bb46018b15a0c679
Arminas19 <armeska14@gmail.com>
2022-09-21 10:51:20 +0000

    created superuser and base.html template, conected template root to settings and made our home-view template

--- trydjagno/views.py
+++ trydjagno/views.py
@@ -1,11 +1,29 @@
 from django.http import HttpResponse
+from articles.models import Article
+from django.template.loader import render_to_string
+import random
 
 
 def home_view(request):
     """ 
         Home / index view 
     """
-    Home_String = '<h1> Hello Home </h1>'
+    random_id = random.randint(1, 3)
 
-    return HttpResponse(Home_String)
+    article_obj = Article.objects.get(id=random_id)
+
+    context = {
+        "title": article_obj.title,
+        "content": article_obj.content,
+        "id": article_obj.id
+    }
+
+    Content_String = render_to_string('home-view.html', context=context)
+
+    # Content_String = """ 
+    # <h1> {title}  ({id})!</h1> 
+    # <p> {content} </p>
+    # """.format(**context)
+
+    return HttpResponse(Content_String)
 
