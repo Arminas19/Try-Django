91a3bc0dc11ed2f79d722c6f8c7317821ccdba42
Arminas19 <armeska14@gmail.com>
2022-09-22 13:33:31 +0000

    created a new detail view for articles and set up the admin panel.

--- trydjagno/views.py
+++ trydjagno/views.py
@@ -4,15 +4,17 @@ from django.template.loader import render_to_string
 import random
 
 
-def home_view(request):
+def home_view(request, *args, **kwargs):
     """ 
         Home / index view 
     """
+    print(id)
     random_id = random.randint(1, 3)
 
     article_obj = Article.objects.get(id=random_id)
-
+    artical_queryset = Article.objects.all()
     context = {
+        "object_list": artical_queryset,
         "title": article_obj.title,
         "content": article_obj.content,
         "id": article_obj.id
