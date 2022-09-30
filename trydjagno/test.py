ab0afb596a709fd9b7e2d9e67ee3b6abb8e3ada4
Arminas19 <armeska14@gmail.com>
2022-09-30 13:05:57 +0000

    writting test with unittest

---
+++ trydjagno/test.py
@@ -0,0 +1,25 @@
+import os
+from django.conf import settings
+from django.test import TestCase 
+from django.contrib.auth.password_validation import validate_password
+
+# Different commands 
+# python3 manage.py test (To run all test's).
+# self.assertTrue(1==1)
+# self.assertEqual(1, 1)
+# self.assertNotEqual(1, 2)
+
+
+class TryDjangoConfigTest(TestCase):
+    def test_secret_key_strength(self):
+        # print('This is the secret key:', settings.SECRET_KEY)
+        # self.assertEqual(settings.SECRET_KEY, 'la2+u2rdceh853y)ddo+7*wpefic%c27%@vu5%l(!hh$#b#)#')
+        try:
+            is_strong = validate_password(settings.SECRET_KEY)
+        except Exception as e:
+            msg = f'Bad Secret_key {e.messages}'
+            self.fail(e)
+            
+
+        
+        
