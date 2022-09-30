5cfe7ac3f49be6e23c163fa66fb1c6db395f9af3
Arminas19 <armeska14@gmail.com>
2022-09-30 13:16:13 +0000

    added another test

--- trydjagno/test.py
+++ trydjagno/test.py
@@ -14,11 +14,14 @@ class TryDjangoConfigTest(TestCase):
     def test_secret_key_strength(self):
         # print('This is the secret key:', settings.SECRET_KEY)
         # self.assertEqual(settings.SECRET_KEY, 'la2+u2rdceh853y)ddo+7*wpefic%c27%@vu5%l(!hh$#b#)#')
+        DATABASE_URL = os.environ.get('DATABASE_URL')
+        print('DATABASE_URL', DATABASE_URL)
         try:
             is_strong = validate_password(settings.SECRET_KEY)
         except Exception as e:
             msg = f'Bad Secret_key {e.messages}'
             self.fail(e)
+
             
 
         
