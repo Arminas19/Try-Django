f1f69e619e06eddfa89869c21bdaa2eea0b9d400
Arminas19 <armeska14@gmail.com>
2022-10-03 15:30:09 +0000

    added another test in articles test.py file

--- articles/models.py
+++ articles/models.py
@@ -22,7 +22,7 @@ class Article(models.Model):
 
 
 def article_pre_save(sender, instance, *args, **kwargs):
-    print('pre_save')
+    # print('pre_save')
     if instance.slug is None:
         slugify_instance_title(instance, save=False)
 
@@ -31,7 +31,7 @@ pre_save.connect(article_pre_save, sender=Article)
 
 
 def article_post_save(sender, instance, created, *args, **kwargs):
-    print('post_save')
+    # print('post_save')
     if created:
         slugify_instance_title(instance, save=True)
 
