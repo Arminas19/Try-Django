80b69eef51ee29243bca16f7f9cb027e7d69723f
Arminas19 <armeska14@gmail.com>
2022-10-06 15:58:55 +0000

    i registered my models to my admin and added some admin classes to customize my admin panel.

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 
+
 class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name =  models.CharField(max_length=220)
