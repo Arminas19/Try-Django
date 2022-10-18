d34cea6e84cff0ebc2e7c3c7bb7960d670ba4a67
Arminas19 <armeska14@gmail.com>
2022-10-18 12:40:26 +0000

    added model formset so that i can use two froms in the same template

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.forms.models import modelformset_factory
 from .models import Recipe, RecipeIngredient
-from .forms import RecipeForm
+from .forms import RecipeForm, RecipeIngredientForm
 # Create your views here.
 
 
@@ -41,11 +42,25 @@ def recipe_create_view(request):
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id, user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
+   # Formset = modelform_factory(Model, form=ModelForm, extra=0)
+    RecipeIngredientFormset = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=0)
+    qs = obj.recipeingredient_set.all()
+    formset = RecipeIngredientFormset(request.POST or None, queryset=qs)
     context = {
         'form': form,
+        'formset': formset,
         "object": obj
     }
-    if form.is_valid():
-        form.save()
+
+    if all([form.is_valid(), formset.is_valid()]):
+        parent = form.save(commit=False)
+        parent.save()
+        # formset.save()
+        for form in formset:
+            child = form.save(commit=False)
+            if child.recipe is None:
+                print("Added New")
+                child.recipe = parent
+            child.save()
         context['message'] = 'Date saved.' 
     return render(request, "recipes/create-update.html", context=context)
