45f9393fdc876d814464ec7b9071a078602a7742
Arminas19 <armeska14@gmail.com>
2022-10-19 14:46:26 +0000

    added widgets to my recipes forms

--- recipes/forms.py
+++ recipes/forms.py
@@ -3,10 +3,28 @@ from .models import Recipe, RecipeIngredient
 
 
 class RecipeForm(forms.ModelForm):
+    error_css_class = 'error-field'
+    required_css_class = 'required-field'
+    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control",
+    "placeholder": "Recipe name"}))
+    # desciption = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
     class Meta:
         model = Recipe 
         fields = ['name', 'desciption', 'direction']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            new_data = {
+                "placeholder": f'Recipe {str(field)}',
+                "class": 'form-control'
+            }
+        self.fields[str(field)].widget.attrs.update(new_data)
+
+        # self.fields['name'].label = ''
+        # self.fields['name'].widget.attrs.update({'class': 'form-control-2'})
+        self.fields['desciption'].widget.attrs.update({'rows': '2'})
+        self.fields['direction'].widget.attrs.update({'rows': '4'})
 
 class RecipeIngredientForm(forms.ModelForm):
     class Meta:
