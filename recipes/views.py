030cc5a1830e8997e02f4e9d2f6b5f3891f78824
Arminas19 <armeska14@gmail.com>
2022-10-23 13:50:43 +0000

    added dynamic forms using HTMX

--- recipes/views.py
+++ recipes/views.py
@@ -60,4 +60,6 @@ def recipe_update_view(request, id=None):
             child.recipe = parent
             child.save()
         context['message'] = 'Date saved.' 
+        if request.htmx:
+            return render(request, "recipes/partials/form.html", context=context)
     return render(request, "recipes/create-update.html", context=context)
